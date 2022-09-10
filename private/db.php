f38aaea474607820eee0923cbb8b78382681ee93
Jack Grayson <jackgraysonfox@gmail.com>
2022-09-10 14:05:15 -0700

    Migrate from MySQLi to PDO for database access

--- private/db.php
+++ private/db.php
@@ -1,32 +1,35 @@
 <?php
 class SQL {
-	private $mysqliconn;
+	private $conn;
+	private $errstr;
 	private $tblprfx;
 	function __construct($hostname, $username, $password, $dbname, $tableprefix) {
-		$this->mysqliconn = new mysqli($hostname, $username, $password);
-		$this->select_db($dbname);
+		$this->conn = new PDO('mysql:host=' . $hostname . ';dbname=' . $dbname, $username, $password);
 		$this->tblprfx = $tableprefix;
 	}
-	function select_db($dbname) {
-		$this->mysqliconn->select_db($dbname);
-	}
-	function query($qstring) {
-		return $this->mysqliconn->query($qstring);
+	function query($qstring, $params = array()) {
+		$statement = $this->conn->query($qstring);
+		if(!$statement) {
+			$this->errstr = implode(' ', $this->conn->errorInfo());
+			return NULL;
+		}
+		return $statement;
 	}
 	function num_rows($result) {
-		return $result->num_rows;
+		return $result->rowCount();
 	}
 	function fetch_assoc($result) {
-		return $result->fetch_assoc();
+		return $result->fetch(PDO::FETCH_ASSOC);
 	}
 	function error() {
-		return $this->mysqliconn->error;
+		return $this->errstr;
 	}
 	function format_table_name($table) {
 		return $this->tblprfx . $table;
 	}
 	function real_escape_string($string) {
-		return $this->mysqliconn->real_escape_string($string);
+		// Really bad hack, this will be replaced with prepared statements
+		return trim($this->conn->quote($string), '"\'');
 	}
 }
 require('db_init.php');
